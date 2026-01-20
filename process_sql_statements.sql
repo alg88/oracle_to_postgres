4792447d62644c8ffcfd68003fb8f9317b7cb834
alg88 <65941503+alg88@users.noreply.github.com>
2026-01-20 12:20:36 +0300

    Add files via upload

---
+++ process_sql_statements.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION migration.process_sql_statements()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    v_rec    RECORD;
+    v_result TEXT;
+BEGIN
+    -- Перебор всех SQL выражений из таблицы
+    FOR v_rec IN SELECT l.sql_text, l.id FROM migration.test_sql_list l LOOP
+        -- Вызов функции и получение результата
+        v_result := migration.test_query(v_rec.sql_text);
+        
+        -- Вывод результата
+--        RAISE NOTICE 'SQL: %, Result: %', v_result, v_rec.sql_text;
+       update migration.test_sql_list l
+         set processed_date  = clock_timestamp()
+             ,errc = case when upper(v_result) = 'OK' then 'S' else 'E' end
+             ,errm = v_result
+       where l.id = v_rec.id;
+    END LOOP;
+END;
+$function$
+;
